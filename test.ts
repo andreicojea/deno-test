f06e57eeb4df271ad60f505090c5c375a10db470
Andrei Cojea <andreicojea@gmail.com>
2021-06-29 17:22:47 +0300

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,3 +1,3 @@
-import { sumBy } from './sumBy.ts';
-
-console.log(sumBy([{ a: 1 }, { a: 2 }], (obj) => obj.a))
+import { sumBy } from 'https://deno.land/x/lodash@4.17.15-es/lodash.js';;
+const {a,b} = JSON.parse(Deno.args[0]);
+console.log(JSON.stringify(sumBy([a, b], (obj:any) => obj.x)));
